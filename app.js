df081112ea2677c1d38d6110fc14cd4bd1c123c6
Andrei <andreicomanescuonline@gmail.com>
2022-02-05 12:50:07 +0200

    Commit

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-//
+//Navbar Window Height and Width
+// 1920 and 185 px
+const widthNavbar = document.querySelector("nav").offsetWidth;
+const heightNavbar = document.querySelector("nav").offsetHeight;
